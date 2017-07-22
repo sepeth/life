adc4d9bb41d5c276dd50bc55e98c997d0b65edd0
Doğan Çeçen <sepeth@gmail.com>
2017-07-22 16:51:13 +0300

    Add clear button and organize the code a bit

--- life.js
+++ life.js
@@ -1,35 +1,3 @@
-let ctx = canvas.getContext('2d');
-
-// Options
-let rowCount = 50;
-let colCount = 50;
-let cellWidth = 10;
-
-// Set canvas size
-canvas.width = rowCount * cellWidth;
-canvas.height = colCount * cellWidth;
-
-
-function parseMap(map) {
-    let lines = map.split('\n').filter(l => l.trim() != "");
-    let ret = [];
-    lines.forEach((line, idx) => {
-        line = line.trim();
-        for (var i = 0; i < line.length; i++) {
-            ret.push(line[i] == "X" ? 1 : 0);
-        }
-        for (; i < colCount; i++) {
-            ret.push(0);
-        }
-    });
-    var missingLines = (rowCount - lines.length) * colCount;
-    for (var i = 0; i < missingLines; i++) {
-        ret.push(0);
-    }
-    return ret;
-}
-
-
 function rAFInterval(fn, delay) {
     var start = window.performance.now();
     var handle = {};
@@ -45,18 +13,27 @@ function rAFInterval(fn, delay) {
 }
 
 
-var Canvas = {
-    clear: function() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    },
+class Grid {
+    constructor(canvas, cellWidth) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.cellWidth = cellWidth;
+    }
 
-    drawGrid: function() {
+    clear() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    draw() {
+        var ctx = this.ctx;
+        this.clear();
+
         // Set the style of the grid lines
         ctx.strokeStyle = '#40e0d0';
-        ctx.lineWidth = 0.25;
+        ctx.lineWidth = .25;
 
         // Draw vertical lines
-        for (var i = cellWidth; i < canvas.width; i += cellWidth) {
+        for (var i = this.cellWidth; i < this.canvas.width; i += this.cellWidth) {
             ctx.beginPath();
             ctx.moveTo(i, 0);
             ctx.lineTo(i, canvas.height);
@@ -65,23 +42,33 @@ var Canvas = {
         }
 
         // Draw horizontal lines
-        for (var i = cellWidth; i < canvas.height; i += cellWidth) {
+        for (var i = this.cellWidth; i < this.canvas.height; i += this.cellWidth) {
             ctx.beginPath();
             ctx.moveTo(0, i);
             ctx.lineTo(canvas.width, i);
             ctx.closePath();
             ctx.stroke();
         }
-    },
+    }
 
-    fill: function (x, y) {
-        ctx.fillStyle = '#40e0d0';
-        ctx.fillRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
-    },
+    fillCell(x, y) {
+        this.ctx.fillStyle = '#40e0d0';
+        this.ctx.fillRect(
+            x * this.cellWidth,
+            y * this.cellWidth,
+            this.cellWidth,
+            this.cellWidth
+        );
+    }
 
-    clearCell: function(x, y) {
-        ctx.fillStyle = '#fff';
-        ctx.fillRect(x * cellWidth + .25, y * cellWidth + .25, cellWidth - .5, cellWidth - .5);
+    clearCell(x, y) {
+        this.ctx.fillStyle = '#fff';
+        this.ctx.fillRect(
+            x * this.cellWidth + .25,
+            y * this.cellWidth + .25,
+            this.cellWidth - .5,
+            this.cellWidth - .5
+        );
     }
 };
 
@@ -91,60 +78,39 @@ class World {
         this.map = map;
     }
 
-    draw() {
-        for (var y = 0; y < rowCount; y++)
-            for (var x = 0; x < colCount; x++)
-                if (this.map[y * colCount + x])
-                    Canvas.fill(x, y);
-    }
-
-    checkCell(x, y) {
-        if (x < 0) {
-            x = x + colCount;
-        } else if (x >= colCount) {
-            x = x % colCount;
-        }
-        if (y < 0) {
-            y = y + rowCount;
-        } else if (y >= rowCount) {
-            y = y % rowCount;
-        }
-        return this.map[y * colCount + x];
-    }
-
     aliveNeighbour(x, y) {
         return (
-            this.checkCell(x-1, y-1) +
-            this.checkCell(x,   y-1) +
-            this.checkCell(x+1, y-1) +
+            this.map.get(x-1, y-1) +
+            this.map.get(x,   y-1) +
+            this.map.get(x+1, y-1) +
 
-            this.checkCell(x-1, y) +
-            this.checkCell(x+1, y) +
+            this.map.get(x-1, y) +
+            this.map.get(x+1, y) +
 
-            this.checkCell(x-1, y+1) +
-            this.checkCell(x,   y+1) +
-            this.checkCell(x+1, y+1)
+            this.map.get(x-1, y+1) +
+            this.map.get(x,   y+1) +
+            this.map.get(x+1, y+1)
         );
     }
 
     tick() {
-        var newMap = this.map.slice();
+        var newMap = this.map.copy();
 
-        for (var y = 0; y < rowCount; y++)
-            for (var x = 0; x < colCount; x++) {
+        for (var y = 0; y < this.map.rowCount; y++)
+            for (var x = 0; x < this.map.colCount; x++) {
                 var nc = this.aliveNeighbour(x, y);
-                if (this.map[y * colCount + x]) {  // alive
+                if (this.map.get(x, y)) {  // alive
                     // any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
                     if (nc < 2)
-                        newMap[y * colCount + x] = 0;
+                        newMap.erase(x, y);
                     // any live cell with more than three live neighbours dies, as if by overpopulation.
                     else if (nc > 3)
-                        newMap[y * colCount + x] = 0;
+                        newMap.erase(x, y);
                     // any live cell with two or three live neighbours lives on to the next generation.
                 } else {  // dead
                     // any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
                     if (nc == 3)
-                        newMap[y * colCount + x] = 1;
+                        newMap.fill(x, y);
                 }
             }
 
@@ -152,16 +118,234 @@ class World {
     }
 
     getMap() {
-        return this.map.slice();
+        return this.map.copy();
     }
 
     setMap(map) {
-        this.map = map.slice();
+        this.map = map.copy();
     }
 }
 
 
-let map = parseMap(`
+class Map {
+    constructor(rowCount, colCount, arr) {
+        if (arr === undefined) {
+            arr = new Array(rowCount * colCount).fill(0);
+        }
+        this.data = arr;
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+    }
+
+    static fromString(rowCount, colCount, mapStr) {
+        let lines = mapStr.split('\n').filter(l => l.trim() != "");
+        let ret = [];
+        lines.forEach((line, idx) => {
+            line = line.trim();
+            for (var i = 0; i < line.length; i++) {
+                ret.push(line[i] == "X" ? 1 : 0);
+            }
+            for (; i < colCount; i++) {
+                ret.push(0);
+            }
+        });
+        var missingLines = (rowCount - lines.length) * colCount;
+        for (var i = 0; i < missingLines; i++) {
+            ret.push(0);
+        }
+        return new Map(rowCount, colCount, ret);
+    }
+
+    copy() {
+        return new Map(this.rowCount, this.colCount, this.data.slice());
+    }
+
+    fill(x, y) {
+        this.data[y * this.colCount + x] = 1;
+    }
+
+    erase(x, y) {
+        this.data[y * this.colCount + x] = 0;
+    }
+
+    get(x, y) {
+        if (x < 0) {
+            x = x + this.colCount;
+        } else if (x >= this.colCount) {
+            x = x % this.colCount;
+        }
+        if (y < 0) {
+            y = y + this.rowCount;
+        } else if (y >= this.rowCount) {
+            y = y % this.rowCount;
+        }
+        return this.data[y * this.colCount + x];
+    }
+}
+
+
+class App {
+    constructor(options) {
+        var canvas = options.canvas;
+
+        // Set canvas size
+        canvas.width = options.colCount * options.cellWidth;
+        canvas.height = options.rowCount * options.cellWidth;
+
+        var initialMap = Map.fromString(
+            options.rowCount,
+            options.colCount,
+            options.initialMapStr
+        );
+
+        this.grid = new Grid(canvas, options.cellWidth);  // Responsible for drawing grid and cells
+        this.world = new World(initialMap);               // Game of life implementation
+        this.editMode = false;                            // is in edit mode or not
+        this.newMap = null;                               // copy of the map if we are in edit mode
+        this.editFill = true;                             // fill color, true == fill, false == clear
+        this.mouseDown = false;                           // is mouse pressed while moving
+        this.prevX = -1;                                  // previous mouse coordinations
+        this.prevY = -1;
+
+        btnPlay.addEventListener('click', _evt => {
+            btnPlay.style.display = 'none';
+            btnPause.style.display = 'inline';
+            if (this.editMode) {
+                this.editMode = false;
+                btnEditMap.disabled = false;
+                this.world.setMap(this.newMap);
+                this.newMap = null;
+            }
+            this.start();
+        });
+
+        btnPause.addEventListener('click', _evt => {
+            this.stop();
+            btnPlay.style.display = 'inline';
+            btnPause.style.display = 'none';
+        });
+
+        btnEditMap.addEventListener('click', _evt => {
+            this.switchToEditMode();
+            this.newMap = this.world.getMap();
+        });
+
+        btnClear.addEventListener('click', _evt => {
+            this.switchToEditMode();
+            // Create an empty map
+            this.newMap = new Map(options.rowCount, options.colCount);
+            // Render an empty grid
+            this.grid.draw();
+        });
+
+        canvas.addEventListener('mousemove', evt => {
+            if (!this.editMode)
+                return;
+            var newX = Math.floor(evt.offsetX / options.cellWidth);
+            var newY = Math.floor(evt.offsetY / options.cellWidth);
+            if (this.mouseDown) {
+                this.colorCell(newX, newY);
+                this.prevX = -1;
+                this.prevY = -1;
+            } else {
+                if (this.prevX != newX || this.prevY != newY) {
+                    if (this.prevX != -1 && this.prevY != -1) {
+                        this.restoreCell(this.prevX, this.prevY);
+                    }
+                    this.flipCell(newX, newY);
+                    this.prevX = newX;
+                    this.prevY = newY;
+                }
+            }
+        });
+
+        canvas.addEventListener('mouseout', _evt => {
+            if (!this.editMode)
+                return;
+            if (this.prevX != -1 && this.prevY != -1) {
+                this.grid.clearCell(this.prevX, this.prevY);
+            }
+            this.prevX = -1;
+            this.prevY = -1;
+        });
+
+        canvas.addEventListener('mousedown', evt => {
+            if (!this.editMode)
+                return;
+            this.mouseDown = true;
+            var newX = Math.floor(evt.offsetX / options.cellWidth);
+            var newY = Math.floor(evt.offsetY / options.cellWidth);
+            this.editFill = !this.newMap.get(newX, newY);
+            this.colorCell(newX, newY);
+        });
+
+        canvas.addEventListener('mouseup', evt => {
+            if (!this.editMode)
+                return;
+            this.mouseDown = false;
+        });
+    }
+
+    switchToEditMode() {
+        this.stop();
+        this.editMode = true;
+        btnEditMap.disabled = true;
+        btnPlay.style.display = 'inline';
+        btnPause.style.display = 'none';
+    }
+
+    colorCell(x, y) {
+        if (this.editFill) {
+            this.grid.fillCell(x, y);
+            this.newMap.fill(x, y);
+        } else {
+            this.grid.clearCell(x, y);
+            this.newMap.erase(x, y);
+        }
+    }
+
+    flipCell(x, y) {
+        if (this.newMap.get(x, y)) {
+            this.grid.clearCell(x, y);
+        } else {
+            this.grid.fillCell(x, y);
+        }
+    }
+
+    restoreCell(x, y) {
+        if (this.newMap.get(x, y)) {
+            this.grid.fillCell(x, y);
+        } else {
+            this.grid.clearCell(x, y);
+        }
+    }
+
+    tick() {
+        this.world.tick();
+        this.draw();
+    }
+
+    draw() {
+        this.grid.draw();
+        var map = this.world.getMap();
+        for (var y = 0; y < map.rowCount; y++)
+            for (var x = 0; x < map.colCount; x++)
+                if (map.get(x, y))
+                    this.grid.fillCell(x, y);
+    }
+
+    start() {
+        this.draw();
+        this.rafHandle = rAFInterval(() => this.tick(), 100);
+    }
+
+    stop() {
+        window.cancelAnimationFrame(this.rafHandle.value);
+    }
+}
+
+
+let initialMapStr = `
 ________________________________________________
 ________________________________________________
 ________________________________________________
@@ -202,140 +386,13 @@ ________________________________XX______________
 ________________________________X_______________
 ________________________________________________
 ________________________________________________
-`);
+`;
 
 
-class App {
-    constructor() {
-        this.world = new World(map);  // Game of life implementation
-        this.editMode = false;        // is in edit mode or not
-        this.newMap = null;           // copy of the map if we are in edit mode
-        this.editFill = true;         // fill color, true == fill, false == clear
-        this.mouseDown = false;       // mouse is pressed while moving
-        this.prevX = -1;              // previous mouse coordinations
-        this.prevY = -1;
-
-        btnPlay.addEventListener('click', _evt => {
-            btnPlay.style.display = 'none';
-            btnPause.style.display = 'inline';
-            if (this.editMode) {
-                this.editMode = false;
-                btnEditMap.disabled = false;
-                this.world.setMap(this.newMap);
-                this.newMap = null;
-            }
-            this.start();
-        });
-
-        btnPause.addEventListener('click', _evt => {
-            this.stop();
-            btnPlay.style.display = 'inline';
-            btnPause.style.display = 'none';
-        });
-
-        btnEditMap.addEventListener('click', _evt => {
-            this.stop();
-            this.editMode = true;
-            btnEditMap.disabled = true;
-            this.newMap = this.world.getMap();
-            btnPlay.style.display = 'inline';
-            btnPause.style.display = 'none';
-        });
-
-        canvas.addEventListener('mousemove', evt => {
-            if (!this.editMode)
-                return;
-            var newX = Math.floor(evt.offsetX / 10);
-            var newY = Math.floor(evt.offsetY / 10);
-            if (this.mouseDown) {
-                this.colorCell(newX, newY);
-                this.prevX = -1;
-                this.prevY = -1;
-            } else {
-                if (this.prevX != newX || this.prevY != newY) {
-                    if (this.prevX != -1 && this.prevY != -1) {
-                        this.restoreCell(this.prevX, this.prevY);
-                    }
-                    this.flipCell(newX, newY);
-                    this.prevX = newX;
-                    this.prevY = newY;
-                }
-            }
-        });
-
-        canvas.addEventListener('mouseout', _evt => {
-            if (!this.editMode)
-                return;
-            if (this.prevX != -1 && this.prevY != -1) {
-                Canvas.clearCell(this.prevX, this.prevY);
-            }
-            this.prevX = -1;
-            this.prevY = -1;
-        });
-
-        canvas.addEventListener('mousedown', evt => {
-            if (!this.editMode)
-                return;
-            this.mouseDown = true;
-            var newX = Math.floor(evt.offsetX / 10);
-            var newY = Math.floor(evt.offsetY / 10);
-            this.editFill = !this.newMap[newY * colCount + newX];
-            this.colorCell(newX, newY);
-        });
-
-        canvas.addEventListener('mouseup', evt => {
-            if (!this.editMode)
-                return;
-            this.mouseDown = false;
-        });
-    }
-
-    colorCell(x, y) {
-        if (this.editFill) {
-            Canvas.fill(x, y);
-            this.newMap[y * colCount + x] = 1;
-        } else {
-            Canvas.clearCell(x, y);
-            this.newMap[y * colCount + x] = 0;
-        }
-    }
-
-    flipCell(x, y) {
-        if (this.newMap[y * colCount + x]) {
-            Canvas.clearCell(x, y);
-        } else {
-            Canvas.fill(x, y);
-        }
-    }
-
-    restoreCell(x, y) {
-        if (this.newMap[y * colCount + x]) {
-            Canvas.fill(x, y);
-        } else {
-            Canvas.clearCell(x, y);
-        }
-    }
-
-    tick() {
-        this.world.tick();
-        this.draw();
-    }
-
-    draw() {
-        Canvas.clear();
-        Canvas.drawGrid();
-        this.world.draw();
-    }
-
-    start() {
-        this.draw();
-        this.rafHandle = rAFInterval(() => this.tick(), 100);
-    }
-
-    stop() {
-        window.cancelAnimationFrame(this.rafHandle.value);
-    }
-}
-
-
-new App().start();
+new App({
+    canvas,
+    initialMapStr,
+    rowCount: 50,
+    colCount: 70,
+    cellWidth: 10
+}).start();
